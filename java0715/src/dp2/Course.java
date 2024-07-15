2977e6bcd15581b687e4f10201da5a5d04e7e138
HyukJin98 <dlgurwls4321@gmail.com>
2024-07-15 16:48:31 +0900

    0715 update

--- java0715/src/dp2/Course.java
+++ java0715/src/dp2/Course.java
@@ -6,5 +6,40 @@ public class Course {
 	
 	private Vector<Transcript> transcript;
 	private String name;
+	
+	public void addTranscript(Transcript t1) {
+		transcript.add(t1);
+		
+	}
+	
+	public void deleteTranscript(Transcript t1) {
+		if(transcript.contains(t1)) {
+			transcript.remove(t1);
+		}
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Course [transcript=" + transcript + ", name=" + name + "]";
+	}
+	public Course(Vector<Transcript> transcript, String name) {
+		super();
+		this.transcript = transcript;
+		this.name = name;
+	}
+	public Vector<Transcript> getTranscript() {
+		return transcript;
+	}
+	public void setTranscript(Vector<Transcript> transcript) {
+		this.transcript = transcript;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 
 }
--- java0715/src/dp2/Student.java
+++ java0715/src/dp2/Student.java
@@ -5,5 +5,33 @@ import java.util.Vector;
 public class Student {
 	private Vector<Transcript> transcript;
 	private String name;
+	public Student(String name) {
+		super();
+		this.transcript = new Vector<Transcript>();;
+		this.name = name;
+	}
+	public void addTranscript(Transcript t1) {
+		transcript.add(t1);
+		
+	}
+	
+	public void deleteTranscript(Transcript t1) {
+		if(transcript.contains(t1)) {
+			transcript.remove(t1);
+		}
+	}
+	public Vector<Transcript> getTranscript() {
+		return transcript;
+	}
+	public void setTranscript(Vector<Transcript> transcript) {
+		this.transcript = transcript;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	
 }
